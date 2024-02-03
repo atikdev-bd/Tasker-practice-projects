ba72830e13c6fbd59f47d699b60992f9129035db
Atikur Rahman <atikdev.bd@gmail.com>
2024-02-03 21:46:07 +0600

    footer add done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 
@@ -6,6 +7,7 @@ function App() {
     <div className="flex justify-center">
       <Header />
       <HeroSection />
+      <Footer />
     </div>
   );
 }
