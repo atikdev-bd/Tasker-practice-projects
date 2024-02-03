bbec39bbe0030e9fe0bcbf6a3052e855aba3539e
Atikur Rahman <atikdev.bd@gmail.com>
2024-02-03 21:09:40 +0600

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-2xl text-indigo-600 font-semibold text-center">
+        this root parent components !
+      </div>
+    </>
+  );
+}
+
+export default App;
